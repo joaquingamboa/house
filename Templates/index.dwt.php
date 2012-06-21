f4ba8e8c3c474a7d0ef18656d51990ef4971d196
joaquingamboa <joaquin.gamboa@inacapmail.cl>
2012-06-21 13:48:01 -0400

    first commit

---
+++ Templates/index.dwt.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Home</title>
+<!-- TemplateEndEditable -->
+<link rel="stylesheet" type="text/css" href="../css/estilos.css"/>
+
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+	<div id="contenedor"> 
+        	<div id="header">            
+        	
+        	</div> 
+            
+            
+
+            <!-- TemplateBeginEditable name="Plantilla" --> 
+                      
+   
+            
+            
+            
+            <div id="contenido">
+            	
+                    
+            </div><!-- TemplateEndEditable -->  
+
+		    <div id="footer">
+              <div id="contacto">Fono: 56-032-254325</div>
+              <div id="contacto">email: drhouse@inacapmail.cl</div>
+              <div id="direccion"> Av. España 2250</div>
+            </div>
+
+
+            
+                                  
+                 
+    </div>
+
+</body>
+
+</html>
